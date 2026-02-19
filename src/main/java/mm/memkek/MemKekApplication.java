39b74cd8ebf3a1c529f0de2b564f7c667837dd08
stanislau.zhvanski <stanislau.zhvanski@idfeurasia.pro>
2026-02-19 09:54:37 +0300

    Initial commit

---
+++ src/main/java/mm/memkek/MemKekApplication.java
@@ -0,0 +1,13 @@
+package mm.memkek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemKekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemKekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mm/memkek/MemKekApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.memkek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemKekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
